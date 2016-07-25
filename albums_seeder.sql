03ebeefa38e64a618797961f4cb70c5ffdf5ee36
Scott Crawford <scottcrawford@Scotts-MacBook-Air-2.local>
2016-07-25 16:08:57 -0500

    SQL update Albums Seeder Exercise

---
+++ albums_seeder.sql
@@ -0,0 +1,10 @@
+USE codeup_test_db;
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+Values ('Michael Jackson', 'Thriller', 1982, 45.5, 'POP'), 
+	('ACDC', 'Back in Black', 1980, 25.9, 'Hard Rock'), 
+	('Pink Floyd', 'Dark Side of Moon', 1973, 22.7, 'Progressive Rock'),
+	('Nirvana', 'Nevermind', 1991, 16.7, 'Grunge'),
+	('The Beatles', 'Sgt Peppers', 1967, 13.1, 'Rock');
+
+
