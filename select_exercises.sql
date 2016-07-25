4a3b4f027466a1b960a7971dfd554e907f72a9aa
Scott Crawford <scottcrawford@Scotts-MacBook-Air-2.local>
2016-07-25 16:45:11 -0500

    Select Exercise Update

---
+++ select_exercises.sql
@@ -0,0 +1,17 @@
+USE codeup_test_db;
+
+
+SELECT 'Pink Floyd' as caption; 
+
+SELECT * from albums where artist = 'Pink Floyd';
+
+SELECT 'SGT Pepper Release' as caption;
+
+SELECT * from albums where name = 'Sgt Peppers';
+
+SELECT 'Nevermind Genre' as caption;
+
+SELECT * from albums where artist = 'Nirvana';
+
+
+
