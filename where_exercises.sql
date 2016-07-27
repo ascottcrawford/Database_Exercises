a848b2ea9e3c2a4a6cdac923551fd9b8b7b7c433
Scott Crawford <scottcrawford@Scotts-MacBook-Air-2.local>
2016-07-27 12:00:39 -0500

    Update WHERE AND

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,13 +1,15 @@
 
 
-SELECT * FROM employees WHERE last_name IN ('Irena', 'Vidya', 'Maya');
+SELECT * FROM employees WHERE first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya';
 
+SELECT * FROM employees WHERE gender = 'm' AND first_name = 'Irena' OR gender = 'm' AND first_name = 'Vidya' OR gender = 'm' AND first_name = 'Maya';
 
 SELECT * FROM employees WHERE last_name LIKE 'E%';
+SELECT * FROM employees WHERE last_name LIKE 'E%' OR last_name LIKE '%e';
+SELECT * FROM employees WHERE last_name LIKE 'E%' AND last_name LIKE '%e';
 
-
-SELECT * FROM employees WHERE birth_date = '1990-12-25' AND '1999-12-31';
+SELECT * FROM employees WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31' AND birth_date Like '%-12-25';
 
 SELECT * FROM employees WHERE birth_date LIKE '%-12-25';
 
-SELECT * FROM employees WHERE last_name LIKE '%q%';
+SELECT * FROM employees WHERE last_name LIKE '%q%' AND NOT last_name LIKE '%qu%';
