c82d678b25b7112add388d3d8f6de125b7059e74
Scott Crawford <scottcrawford@Scotts-MacBook-Air-2.local>
2016-07-27 10:06:24 -0500

    Update Where SQL Exercises

---
+++ where_exercises.sql
@@ -0,0 +1,13 @@
+
+
+SELECT * FROM employees WHERE last_name IN ('Irena', 'Vidya', 'Maya');
+
+
+SELECT * FROM employees WHERE last_name LIKE 'E%';
+
+
+SELECT * FROM employees WHERE birth_date = '1990-12-25' AND '1999-12-31';
+
+SELECT * FROM employees WHERE birth_date LIKE '%-12-25';
+
+SELECT * FROM employees WHERE last_name LIKE '%q%';
