15e90a67923f4d821aca3681e0296abef60c369d
Scott Crawford <scottcrawford@Scotts-MacBook-Air-2.local>
2016-08-01 08:32:46 -0500

    Updates to SQL Exercises

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -1,3 +1,13 @@
+SELECT COUNT(*) FROM employees WHERE gender = 'm' AND first_name = 'Irena' OR gender = 'm' AND first_name = 'Vidya' OR gender = 'm' AND first_name = 'Maya';
+
+SELECT COUNT(*) FROM employees WHERE gender = 'f' AND first_name = 'Irena' OR gender = 'f' AND first_name = 'Vidya' OR gender = 'f' AND first_name = 'Maya';
+
+SELECT CONCAT (first_name, '', last_name) FROM employees WHERE last_name LIKE 'E%e';
+
+SELECT first_name, last_name, DATEDIFF(curdate(), hire_date)  FROM employees WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31' AND birth_date Like '%-12-25';
+
+
+SELECT COUNT(last_name) as COUNT, CONCAT(first_name, ' ', last_name) as full_name FROM employees WHERE last_name LIKE '%q%' AND NOT last_name LIKE '%qu%' GROUP BY full_name ORDER BY COUNT DESC;
 
 
 -- SELECT * FROM employees WHERE first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya';
@@ -38,9 +48,3 @@
 -- ORDER BY birth_date ASC, hire_date DESC;
 
 
-SELECT COUNT(*) FROM employees WHERE gender = 'm' AND first_name = 'Irena' OR gender = 'm' AND first_name = 'Vidya' OR gender = 'm' AND first_name = 'Maya';
-SELECT COUNT(*) FROM employees WHERE gender = 'f' AND first_name = 'Irena' OR gender = 'f' AND first_name = 'Vidya' OR gender = 'f' AND first_name = 'Maya';
-SELECT CONCAT (first_name, '', last_name) FROM employees WHERE last_name LIKE 'E%e';
-SELECT DATEDIFF(curdate(), hire_date) FROM employees WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31' AND birth_date Like '%-12-25';
-
-SELECT COUNT(*) FROM employees WHERE last_name LIKE '%q%' AND NOT last_name LIKE '%qu%' ORDER BY last_name ASC;
